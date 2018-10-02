22fac7106fe75d69a4cc8a98bc0dce5c2301f983
7hills <ch.7hills@gmail.com>
2018-10-02 01:33:03 -0700

    my test commit message from postman

--- SamplePostAPI.py
+++ SamplePostAPI.py
@@ -1,11 +1,11 @@
 import requests,json,time
 print "***********************started**************************",str(time.asctime())
 #{msp_code=msp_2802, number=T20180725.0001, id=20634}
-resHed = {'content-type': "application/json"}
-#url = "https://stg.netenrichgo.com/incident/autotask/callback?msp_code=msp_2802&id=20674&number=T20180801.0006"
-#url = "http://f1.netenrich.us/incident/connectwise/callback?msp_code=msp_570823&id=5362&number=5362"
-for i in xrange(10):
-	url = "https://stg.netenrichgo.com/incident/connectwise/callback?msp_code=msp_570823&id=6001&number=6001"
+resHed = {'content-type': "application/vnd.github.v3+json",'Authorization':"token d8ec1c3273d73fde38d755fdcf24e781dd1066b9"}
+body={"message": "my test commit message from postman","committer": {"name": "ch7hills","email": "ch.7hills@gmail.com"},"content": "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz","sha": "329688480d39049927147c162b9d2deaf885005f"}
+for i in xrange(1):
+	#url = "https://stg.netenrichgo.com/incident/connectwise/callback?msp_code=msp_570823&id=6001&number=6001"
+	url ="https://api.github.com/repos/ch7hills/dosa/contents/SamplePostAPI.py"
 	#url = "http://localhost:8505/incident/connectwise/callback?msp_code=msp_570823&id=5362"+str(i)+"&number=5362"+str(i)
-	result=requests.get(url, headers={}, verify = False)
+	result=requests.get(url, headers=resHed, data={}, verify = True)
 	print result,"RESULT--->--->",result.content
